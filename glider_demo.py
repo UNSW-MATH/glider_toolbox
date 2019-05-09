2179814968e6e0ae0ea61e72439653e77fb05cf4
Michael Hemming <z3526971@matht290.maths.unsw.edu.au>
2019-05-09 15:12:23 +1000

    python script demonstrating glider toolbox

---
+++ glider_demo.py
@@ -0,0 +1,29 @@
+#--------------------------------------------
+# import functions
+from glider_toolbox import glider_concat
+from glider_toolbox import find_chunks
+
+#-------------------------------------------- 
+# define local and data directory
+local_dir = "/srv/scratch/z3526971/"
+data_dir = local_dir + 'IMOS_gliders/'
+
+#--------------------------------------------
+# define best chunks for .zarr file
+glider_chunks = find_chunks(data_dir)
+
+#--------------------------------------------
+# save concatenated glider data as netCDF and zarr files in 
+# data directory
+file_name = 'Concat_glider'
+data_files = glider_concat(data_dir,glider_chunks,file_name)
+
+#--------------------------------------------
+
+
+
+
+
+
+
+
