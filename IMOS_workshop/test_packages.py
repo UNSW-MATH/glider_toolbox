76b62e328d79454b26ce58817419cd1afa1018ab
Michael Hemming <m.hemming@unsw.edu.au>
2019-06-10 11:58:58 +0930

    Latest version of IMOS workshop glider folder

---
+++ IMOS_workshop/test_packages.py
@@ -0,0 +1,10 @@
+import xarray
+import pandas
+import dask
+import cmocean
+import cartopy
+import jupyter
+import netCDF4
+print('------------------------------')
+print('Packages are working :-)')
+print('------------------------------')
